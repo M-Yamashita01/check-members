80990cf049085b7cbceeb81c404f80e31706e1d8
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-11-18 09:03:02 +0900

    Show outputs in GitHub Actions. Use local actions in GitHub Actions workflow.

--- lib/index.rb
+++ lib/index.rb
@@ -1,7 +1,5 @@
 require 'octokit'
 
-puts 'Hello'
-puts ENV['INPUT_GITHUB_TOKEN']
 access_token = ENV['INPUT_GITHUB_TOKEN']
 
 client = Octokit::Client.new(access_token: access_token)
@@ -10,5 +8,5 @@ org_plan = organization[:plan]
 filled_seats = org_plan['filled_seats']
 seats = org_plan['seats']
 
-puts "filled_seats: #{filled_seats}"
-puts "seats: #{seats}"
+puts "::set-output name=filled_seats::#{filled_seats}"
+puts "::set-output name=seats::#{seats}"
