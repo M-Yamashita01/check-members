2737c33d0facacd8166b791f961a4c2a1f9470bc
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-11-19 06:47:25 +0900

    Add rescue.

--- lib/index.rb
+++ lib/index.rb
@@ -2,11 +2,16 @@ require 'octokit'
 
 access_token = ENV['INPUT_GITHUB_TOKEN']
 
-client = Octokit::Client.new(access_token: access_token)
-organization = client.org('my-organization-sandbox')
-org_plan = organization[:plan]
-filled_seats = org_plan['filled_seats']
-seats = org_plan['seats']
+begin
+  client = Octokit::Client.new(access_token: access_token)
+  organization = client.org('my-organization-sandbox')
+  org_plan = organization[:plan]
+  filled_seats = org_plan['filled_seats']
+  seats = org_plan['seats']
 
-puts "::set-output name=filled_seats::#{filled_seats}"
-puts "::set-output name=seats::#{seats}"
+  puts "::set-output name=filled_seats::#{filled_seats}"
+  puts "::set-output name=seats::#{seats}"
+rescue => e
+  puts '[ERROR] This actions is finished with error.'
+  puts e.backtrace
+end
