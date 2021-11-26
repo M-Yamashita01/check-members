9bb36c0bad4406aea712a636a44ddb16103fb8c3
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-11-27 07:48:27 +0900

    Validated environment variables.
    There were no validation for environment variables, so that we created ConfigValidator class and spec.

---
+++ spec/config_validator_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+RSpec.describe ConfigValidator do
+  describe '#validate?' do
+    let(:access_token) { 'Sample Access Token' }
+    let(:membership_file_path) { "#{__dir__}/fixtures/membership.tf" }
+    let(:repository_collaborator_file_path) { "#{__dir__}/fixtures/repository_collaborator.tf" }
+
+    let(:config_validator) do
+      ConfigValidator.new(
+        access_token: access_token,
+        membership_file_path: membership_file_path,
+        repository_collaborator_file_path: repository_collaborator_file_path)
+    end
+
+    subject { config_validator.validate }
+
+    context 'All environment variables are valid.' do
+      let(:access_token) { 'Sample Access Token' }
+      let(:membership_file_path) { "#{__dir__}/fixtures/membership.tf" }
+      let(:repository_collaborator_file_path) { "#{__dir__}/fixtures/repository_collaborator.tf" }
+
+      it { expect(config_validator.validate?).to be_truthy }
+    end
+
+    context 'Access token is empty' do
+      let(:access_token) { '' }
+
+      it { expect(config_validator.validate?).to be_falsey }
+    end
+
+    context 'Access token is nil' do
+      let(:access_token) { nil }
+
+      it { expect(config_validator.validate?).to be_falsey }
+    end
+
+    context 'Membership file path is empty' do
+      let(:membership_file_path) { '' }
+
+      it { expect(config_validator.validate?).to be_falsey }
+    end
+
+    context 'Repository Collaborator file path is nil' do
+      let(:repository_collaborator_file_path) { '' }
+
+      it { expect(config_validator.validate?).to be_falsey }
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,4 @@ require 'rspec'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'terraform_reader'
+require 'config_validator'
